fb93c9eb2fac828aee26ab91f5c518d93c811252
ironsilk <mihai.vlad6@gmail.com>
2023-05-20 10:15:08 +0300

    Database interraction
    Implemented db fetching/creating/deleting queries

--- resources/db.py
+++ resources/db.py
@@ -1,4 +1,6 @@
 import os
+from datetime import timedelta
+
 from dotenv import load_dotenv
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.exc import SQLAlchemyError
@@ -9,6 +11,7 @@ load_dotenv()
 session = db.session
 
 
+# Generic functions
 def db_insert_multiple_conflict(table, items, key='id'):
     """
     Insert multiple rows into a table, ignoring conflicts
@@ -27,14 +30,10 @@ def db_insert_multiple_conflict(table, items, key='id'):
         raise e
 
 
-def db_get_turbine_last_entry(turbine_id):
+# DataLog
+def db_get_turbine_datalog_last_entry(turbine_id):
     # Get last entry by timestamp
     return session.query(DataLog).filter_by(turbine_id=turbine_id).order_by(DataLog.job_timestamp.desc()).first()
-
-
-def db_insert_turbine_data(items):
-    # Insert rows into database
-    db_insert_multiple_conflict(TurbineData, items)
 
 
 def db_insert_datalog(item):
@@ -46,6 +45,73 @@ def db_insert_datalog(item):
     except SQLAlchemyError as e:
         session.rollback()
         raise e
+
+
+# TurbineData
+def db_get_turbine_first_entry(turbine_id):
+    # Get last entry by timestamp
+    return session.query(TurbineData).filter_by(turbine_id=turbine_id).order_by(TurbineData.timestamp.asc()).first()
+
+
+def db_get_turbine_data_interval(turbine_id, timestamp, compute_time_window):
+    # Calculate the timestamp range using timedelta
+    start_timestamp = timestamp - timedelta(seconds=compute_time_window)
+
+    # Get the data within the timestamp range and for the specific turbine_id
+    return session.query(TurbineData).filter(
+        TurbineData.turbine_id == turbine_id,
+        TurbineData.timestamp.between(start_timestamp, timestamp)
+    ).all()
+
+
+def db_insert_turbine_data(items):
+    # Insert rows into database
+    db_insert_multiple_conflict(TurbineData, items)
+
+
+def db_wipe_turbine_data(turbine_id, timestamp):
+    # Delete all rows within the timestamp range and for the specific turbine_id
+    session.query(TurbineData).filter(
+        TurbineData.turbine_id == turbine_id,
+        TurbineData.timestamp <= timestamp
+    ).delete()
+    session.commit()
+
+
+# ComputedData
+def db_get_computed_data(turbine_id):
+    # Get last entry by timestamp
+    return session.query(ComputedData).filter_by(turbine_id=turbine_id)\
+        .order_by(ComputedData.timestamp.desc()).all()
+
+
+def db_get_computed_last_entry(turbine_id):
+    # Get last entry by timestamp
+    return session.query(ComputedData).filter_by(turbine_id=turbine_id)\
+        .order_by(ComputedData.timestamp.desc()).first()
+
+
+def db_insert_computed_data(item):
+    # Insert rows into database
+    data = ComputedData(**item)
+    session.add(data)
+    try:
+        session.commit()
+        return data
+    except SQLAlchemyError as e:
+        session.rollback()
+        raise e
+
+
+# Anomaly
+def db_get_anomalies():
+    # Get all anomalies
+    return session.query(Anomaly).all()
+
+
+def db_insert_anomalies(items):
+    # Insert rows into database
+    db_insert_multiple_conflict(Anomaly, items)
 
 
 
